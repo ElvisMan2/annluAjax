73396f30f252392a59a07c7da2922c217e3d27b1
ElvisMan2 <104602581+ElvisMan2@users.noreply.github.com>
2024-05-21 22:56:26 -0500

    ajax

---
+++ ajax.js
@@ -0,0 +1,27 @@
+function htmlAjax(nombreArchivo, idhtml){
+    
+    const request= new XMLHttpRequest();
+    const url='./html/'+nombreArchivo;
+
+    request.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+            
+            document.getElementById(idhtml).outerHTML=this.responseText;
+              
+        }
+    }
+    
+    request.open("GET", url);
+    request.send();
+
+}
+
+window.addEventListener('load', () => {
+    htmlAjax('nav.html','nav');
+    htmlAjax('carousel.html','carousel');
+    htmlAjax('footer.html','footer');
+    htmlAjax('contenido.html','contenido');
+});
+
+
+
